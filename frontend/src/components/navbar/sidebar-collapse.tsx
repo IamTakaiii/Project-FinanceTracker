a5724c37ad568f73c1fc1a866ad9f51a38a311b3
TakaiNipas <nipas.devs@gmail.com>
2025-07-25 10:08:10 +0700

    refactor: improve LoginForm structure and enhance error handling; update navigation links

--- frontend/src/components/navbar/sidebar-collapse.tsx
+++ frontend/src/components/navbar/sidebar-collapse.tsx
@@ -1,102 +1,105 @@
 import * as React from "react";
 import { ChevronRight } from "lucide-react";
+import { Link, useRouterState } from "@tanstack/react-router";
 
 import { SearchForm } from "@/components/navbar/form-search";
 import { VersionSwitcher } from "@/components/navbar/version-switcher";
+import { NavUser } from "./nav-user";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarGroupLabel,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	SidebarRail,
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
 } from "@/components/ui/sidebar";
-import { Link, useRouterState } from "@tanstack/react-router";
-import { NavUser } from "./nav-user";
 import { AuthStore } from "@/stores/auth";
 
-type AdditonalSidebarProps = {
-	navList: {
-		versions: string[];
-		navMain: {
-			title: string;
-			url: string;
-			items: {
-				title: string;
-				url: string;
-			}[];
-		}[];
-	};
+type AdditionalSidebarProps = {
+    navList: {
+        versions: string[];
+        navMain: {
+            title: string;
+            url: string;
+            items: {
+                title: string;
+                url: string;
+            }[];
+        }[];
+    };
 };
-type SidebarProps = React.ComponentProps<typeof Sidebar> & AdditonalSidebarProps;
+type SidebarProps = React.ComponentProps<typeof Sidebar> & AdditionalSidebarProps;
 
-export function AppSidebar({ ...props }: SidebarProps) {
-	const { pathname } = useRouterState({ select: (s) => s.location });
-	const auth = AuthStore((state) => state);
+export function AppSidebar({ navList, ...props }: SidebarProps) {
+    const { pathname } = useRouterState({ select: (s) => s.location });
 	
-	return (
-		<Sidebar {...props}>
-			<SidebarHeader>
-				<VersionSwitcher versions={props.navList.versions} defaultVersion={props.navList.versions[0]} />
-				<SearchForm />
-			</SidebarHeader>
-			<SidebarContent className="gap-0">
-				{props.navList.navMain.map((item) => (
-					<Collapsible key={item.title} title={item.title} className="group/collapsible">
-						<SidebarGroup>
-							<SidebarGroupLabel
-								asChild
-								className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
-							>
-								<CollapsibleTrigger className="group-data-[state=open]/collapsible:!font-bold group-data-[state=open]/collapsible:!text-primary">
-									{item.title}{" "}
-									<ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
-								</CollapsibleTrigger>
-							</SidebarGroupLabel>
-							<CollapsibleContent>
-								<SidebarGroupContent>
-									<SidebarMenu>
-										{item.items.map((menuItem) => (
-											<SidebarMenuItem key={menuItem.title}>
-												<SidebarMenuButton asChild isActive={pathname === menuItem.url}>
-													<Link to={menuItem.url}>{menuItem.title}</Link>
-												</SidebarMenuButton>
-											</SidebarMenuItem>
-										))}
-									</SidebarMenu>
-								</SidebarGroupContent>
-							</CollapsibleContent>
-						</SidebarGroup>
-					</Collapsible>
-				))}
+    const { isAuthenticated, user } = AuthStore((state) => ({
+        isAuthenticated: state.isAuthenticated,
+        user: state.user,
+    }));
 
-			</SidebarContent>
-			<SidebarRail />
-			<SidebarFooter>
-				{
-					auth.isAuthenticated ? (
-						<NavUser user={{ 
-							name: auth.user?.name || "Anonymous User",
-							email: auth.user?.email || "-",
-							avatar: auth.user?.image || "https://via.placeholder.com/150"
-						}} />
-					) : (
-						<SidebarMenu>
-							<SidebarMenuItem>
-								<SidebarMenuButton asChild>
-									<Link to="/login">Login</Link>
-								</SidebarMenuButton>
-							</SidebarMenuItem>
-						</SidebarMenu>
-					)
-				}
-			</SidebarFooter>
-		</Sidebar>
-	);
+    return (
+        <Sidebar {...props}>
+            <SidebarHeader>
+                <VersionSwitcher versions={navList.versions} defaultVersion={navList.versions?.[0] || "N/A"} />
+                <SearchForm />
+            </SidebarHeader>
+            <SidebarContent className="gap-0">
+                {navList.navMain.map((item) => (
+                    <Collapsible key={item.title} title={item.title} className="group/collapsible">
+                        <SidebarGroup>
+                            <SidebarGroupLabel
+                                asChild
+                                className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
+                            >
+                                <CollapsibleTrigger className="group-data-[state=open]/collapsible:!font-bold group-data-[state=open]/collapsible:!text-primary">
+                                    {item.title}{" "}
+                                    <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                                </CollapsibleTrigger>
+                            </SidebarGroupLabel>
+                            <CollapsibleContent>
+                                <SidebarGroupContent>
+                                    <SidebarMenu>
+                                        {item.items.map((menuItem) => (
+                                            <SidebarMenuItem key={menuItem.title}>
+                                                <SidebarMenuButton asChild isActive={pathname.startsWith(menuItem.url)}>
+                                                    <Link to={menuItem.url}>{menuItem.title}</Link>
+                                                </SidebarMenuButton>
+                                            </SidebarMenuItem>
+                                        ))}
+                                    </SidebarMenu>
+                                </SidebarGroupContent>
+                            </CollapsibleContent>
+                        </SidebarGroup>
+                    </Collapsible>
+                ))}
+            </SidebarContent>
+            <SidebarRail />
+            <SidebarFooter>
+                {isAuthenticated ? (
+                    <NavUser
+                        user={{
+                            name: user?.name || "Anonymous User",
+                            email: user?.email || "-",
+                            avatar: user?.image || "https://via.placeholder.com/150",
+                        }}
+                    />
+                ) : (
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton asChild>
+                                <Link to="/login">Login</Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                )}
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
